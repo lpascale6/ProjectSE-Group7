1c85cc56a6aded0e633d48ae94e1aa40ac9efcf4
luigi <luigi@OMENdiLuigi.lan>
2022-11-15 10:42:14 +0100

    First commit

---
+++ ProjectSE_Group7/src/projectse_group7/FXMLDocumentController.java
@@ -0,0 +1,30 @@
+package projectse_group7;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author group7
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello world from Luigi Vicidomini!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
