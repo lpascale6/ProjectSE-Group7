294f4be494c0c97eb3daa80fee8dfde78c6e292d
luigi <luigi@OMENdiLuigi.lan>
2022-11-23 17:41:08 +0100

    Implemented GUI and toggle buttons behavior.

--- ProjectSE_Group7/src/projectse_group7/FXMLDocumentController.java
+++ ProjectSE_Group7/src/projectse_group7/FXMLDocumentController.java
@@ -2,29 +2,54 @@ package projectse_group7;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
 
 /**
  *
  * @author group7
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private ToggleButton lineToggleButton;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello world from Paolo!!!");
+    private ToggleButton rectangleToggleButton;
+    @FXML
+    private ToggleButton ellipseToggleButton;
+    @FXML
+    private Pane drawingPane;
+    
+    // toggle group variable, useful for selecting only 
+    // once toggle button at a time
+    ToggleGroup shapeToggleGroup;
+
+    /**
+     * Sets up all the toggle buttons in the "Shape selection" section 
+     * by creating a toggle group.
+     */
+    private void setupShapeToggleButtons() {
+        // creating a toggle group and inserting all the toggle buttons in it
+        shapeToggleGroup = new ToggleGroup();
+        lineToggleButton.setToggleGroup(shapeToggleGroup);
+        rectangleToggleButton.setToggleGroup(shapeToggleGroup);
+        ellipseToggleButton.setToggleGroup(shapeToggleGroup);
+        
+        // to make sure that a toggle button in the group is always selected
+        shapeToggleGroup.selectedToggleProperty().addListener((obsValue, oldValue, newValue) -> {
+            if (newValue == null)
+                oldValue.setSelected(true);
+        });
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        // setting up all toggle buttons
+        setupShapeToggleButtons();
+    }
+
 }
