84abfc1f982f955995a9073235cb6a21e93c3a31
luigi <luigi@OMENdiLuigi.lan>
2022-11-25 16:24:33 +0100

    Created Command interface in command package.

---
+++ ProjectSE_Group7/src/command/Command.java
@@ -0,0 +1,12 @@
+package command;
+
+/**
+ * This is the interface used to implement the command operations.
+ * 
+ * @author group7
+ */
+public interface Command {
+    
+    public void execute() throws Exception;
+    
+}
