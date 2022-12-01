37b63ce63ac78b5d4c9b20a0cf2fdbda9c2ce203
LUIGI PASCALE <l.pascale6@studenti.unisa.it>
2022-12-01 11:17:26 +0100

    Added comments to ChangeFillColorCommand and ChangeOutlineColorCommand

--- ProjectSE_Group7/src/command/ChangeFillColorCommand.java
+++ ProjectSE_Group7/src/command/ChangeFillColorCommand.java
@@ -12,17 +12,30 @@ public class ChangeFillColorCommand implements Command{
     Color newColor;
     Color previousColor;
 
+    /**
+     * method that creates a change fill color command.
+     * @param shape the shape that has to get its fill color changed.
+     * @param newColor the new value for the shape fill color.
+     */
     public ChangeFillColorCommand(Shape shape, Color newColor) {
         this.shape = shape;
         this.newColor = newColor;
         this.previousColor = (Color) this.shape.getFill();
     }
 
+    /**
+     * method that changes the shape fill color.
+     * @throws Exception 
+     */
     @Override
     public void execute() throws Exception {
         this.shape.setFill(this.newColor);
     }
 
+    /**
+     * method that sets the shape fill color to the previous value.
+     * @throws Exception 
+     */
     @Override
     public void undo() throws Exception {
         this.shape.setFill(this.previousColor);
--- ProjectSE_Group7/src/command/ChangeOutlineColorCommand.java
+++ ProjectSE_Group7/src/command/ChangeOutlineColorCommand.java
@@ -12,17 +12,30 @@ public class ChangeOutlineColorCommand implements Command{
     Color newColor;
     Color previousColor;
 
+    /**
+     * method that creates a change outline color command.
+     * @param shape the shape that has to get its outline color changed.
+     * @param newColor the new value for the shape outline color.
+     */
     public ChangeOutlineColorCommand(Shape shape, Color newColor) {
         this.shape = shape;
         this.newColor = newColor;
         this.previousColor = (Color) this.shape.getStroke();
     }
 
+    /**
+     * method that changes the shape outline color.
+     * @throws Exception 
+     */
     @Override
     public void execute() throws Exception {
         this.shape.setStroke(this.newColor);
     }
 
+    /**
+     * method that sets the shape outline color to the previous value.
+     * @throws Exception 
+     */
     @Override
     public void undo() throws Exception {
         this.shape.setStroke(this.previousColor);
